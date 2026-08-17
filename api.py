6f654c11dc0e3bf30add859adc19fe1ba623b8fe
Amaya <amaya@MacBook-Pro-9.local>
2026-08-17 01:38:17 -0700

    Allow Vercel frontend

--- api.py
+++ api.py
@@ -15,10 +15,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.1.80:3000",
-    ],
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.1.80:3000",
+    "https://matchline-pearl.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
